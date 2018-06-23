35b6bea3904f5d5bb977a73dd32ac692e08a9e90
bar-lb <bar@lightbitslabs.com>
2018-06-23 20:25:18 +0300

    added the first monster - the Dark Magician

---
+++ yugiho/monsters/DarkMagician.java
@@ -0,0 +1,11 @@
+package yugiho.monsters;
+
+public class DarkMagician extends MonsterCard{
+	public DarkMagician(){
+		super(	"Dark Magician",
+				"A powerful wizard",
+				2500,
+				2100,
+				7 );
+	}
+}
